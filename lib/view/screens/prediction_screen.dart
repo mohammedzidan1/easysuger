29c3c78fefc4b2cfd6287d8c609c3da34e8186e9
mohammedzidan <mohmmedzidane@gmail.com>
2022-05-11 02:11:32 +0300

    update constants , approuts and prefiction screen

--- lib/view/screens/prediction_screen.dart
+++ lib/view/screens/prediction_screen.dart
@@ -1,35 +1,40 @@
 import 'package:easysugar/help/my_colors_app.dart';
 import 'package:easysugar/help/routs/routs_name.dart';
 import 'package:easysugar/view/custom_widet/custom_drawer.dart';
+import 'package:easysugar/view/custom_widet/custom_drob_down_menu.dart';
+import 'package:easysugar/view/custom_widet/custom_drob_menue_long_action.dart';
 import 'package:easysugar/view/custom_widet/custom_text.dart';
+import 'package:easysugar/view/custom_widet/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 import '../custom_widet/custom_add_prediction.dart';
 
+// ignore: must_be_immutable
 class PredictionScreen extends StatefulWidget {
-  const PredictionScreen({Key? key}) : super(key: key);
+  PredictionScreen({Key? key}) : super(key: key);
 
   @override
   State<PredictionScreen> createState() => _PredictionScreenState();
 }
 
 class _PredictionScreenState extends State<PredictionScreen> {
-  // var scaffoldKey = GlobalKey<ScaffoldState>();
-  // var formKey = GlobalKey<FormState>();
-  // var activityController = TextEditingController();
-  // var lanchController = TextEditingController();
-  // var dinnerController = TextEditingController();
+  String selectedItemTypeOfFood = "lunch";
+  List<String> typeOfFoodList = ["lunch", "dinner", "breakfast"];
+  var mealController = TextEditingController();
 
-  var breakfastController = TextEditingController();
+  var activityController = TextEditingController();
+  var durationControllar = TextEditingController();
 
   var glucoseController = TextEditingController();
 
   var timeController = TextEditingController();
+
   var dateController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    //  key: scaffoldKey,
+      //  key: scaffoldKey,
       appBar: AppBar(
           backgroundColor: ColorsApp.primaryColor,
           title: const CustomText(
@@ -49,7 +54,6 @@ class _PredictionScreenState extends State<PredictionScreen> {
           child: SingleChildScrollView(
             child: Column(children: [
               CustomAddPrediction(
-                
                 readOnly: true,
                 onTap: () {
                   showDatePicker(
@@ -81,19 +85,19 @@ class _PredictionScreenState extends State<PredictionScreen> {
                   },
                   lableText: "Time",
                   assetsImage: "assets/images/icons8-music-time-100.png"),
-              CustomAddPrediction(
-                onTap: () {
-                  // scaffoldKey.currentState!.showBottomSheet(
-                  //   (context) => Container(
-                  //     color: Colors.white,
-                  //     padding: const EdgeInsets.all(
-                  //       20.0,
-                  //     ),
-                  //   ),
-                  // );
-
-                 
+              DropdownButton<String>(
+                value: selectedItemTypeOfFood,
+                items: typeOfFoodList.map((e) {
+                  return DropdownMenuItem(value: e, child: Text(e));
+                }).toList(),
+                onChanged: (String? value) {
+                  setState(() {
+                    selectedItemTypeOfFood = value!;
+                  });
                 },
+              ),
+              CustomAddPrediction(
+                onTap: () {},
                 assetsImage: "assets/images/icons8-meal-100.png",
                 lableText: "Meal",
               ),
@@ -102,12 +106,48 @@ class _PredictionScreenState extends State<PredictionScreen> {
                 assetsImage: "assets/images/icons8-activity-64.png",
                 lableText: "Activity type",
               ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              CustomTextField(
+                controller: durationControllar,
+                lableText: "duration ?",
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
               CustomAddPrediction(
-                onTap: () {
-               
-                },
+                onTap: () {},
                 assetsImage: "assets/images/icons8-insulin-pen-48.png",
                 lableText: "Glucose measurement",
+              ),
+              Row(
+                children: const [
+                  CustomText(
+                    text: "Short Action",
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(child: CustomDrobDownMenuShortAction()),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: const [
+                  CustomText(
+                      text: "Long Action",
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(child: CustomDrobDownMenuLongAction()),
+                ],
               ),
             ]),
           ),
@@ -121,7 +161,7 @@ class _PredictionScreenState extends State<PredictionScreen> {
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      drawer:const CustomDrawer(),
+      drawer: const CustomDrawer(),
     );
   }
 }
