5589cb369ab7aabf3f734f81f465a62dc7f75c5f
mohammedzidan <mohmmedzidane@gmail.com>
2022-05-12 20:44:49 +0300

    update add record

--- lib/view/screens/add_record_screen.dart
+++ lib/view/screens/add_record_screen.dart
@@ -1,20 +1,33 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:easysugar/help/my_colors_app.dart';
 import 'package:easysugar/help/routs/routs_name.dart';
-import 'package:easysugar/view/custom_widet/custom_background_curve.dart';
-import 'package:easysugar/view/custom_widet/custom_button.dart';
 import 'package:easysugar/view/custom_widet/custom_default_button.dart';
 import 'package:easysugar/view/custom_widet/custom_drob_menue_long_action.dart';
 import 'package:easysugar/view/custom_widet/custom_text.dart';
+import 'package:easysugar/view/custom_widet/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 import '../custom_widet/custom_drob_down_menu.dart';
-import '../custom_widet/custom_text_field.dart';
 
-class AddRecordScreen extends StatelessWidget {
+class AddRecordScreen extends StatefulWidget {
   AddRecordScreen({Key? key}) : super(key: key);
 
+  @override
+  State<AddRecordScreen> createState() => _AddRecordScreenState();
+}
+
+class _AddRecordScreenState extends State<AddRecordScreen> {
+  String selectedItemPills = "Metformin Tablets";
+    List<String> pillsList = ["Metformin Tablets", "Omformin", "Glucophage 100 mg","other"];
+
   var timeController = TextEditingController();
+  var otheController = TextEditingController();
+
   var dateController = TextEditingController();
+
+  var fastingController = TextEditingController();
+
+  var reminderController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -24,96 +37,176 @@ class AddRecordScreen extends StatelessWidget {
         title: const CustomText(
           text: "Add Record",
         ),
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.pushNamed(context, RoutsNames.homeScreenForPatient);
-              },
-              icon: const Icon(Icons.home))
-        ],
+        // actions: [
+        //   IconButton(
+        //       onPressed: () {
+        //         Navigator.pushNamed(context, RoutsNames.homeScreenForPatient);
+        //       },
+        //       icon: const Icon(Icons.home))
+        // ],
       ),
-      body: Column(children: [
-        //CustomTextField(width: 100,),
-        Row(
-          children: [
-            CustomDefaultButton(
-              height: 50,
-              text: "Date",
-              ontap: () {
-                showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime.now(),
-                  lastDate: DateTime.parse('2030-10-03'),
-                ).then((value) {
-                  dateController.text = value.toString();
-                });
-                print("object");
-              },
-              // color: Colors.grey[400],
-              width: 100,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(children: [
+           
+            Row(
+              children: [
+                CustomDefaultButton(
+                  height: 50,
+                  text: "Date",
+                  ontap: () {
+                     showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime.now(),
+                    lastDate: DateTime.parse('2030-10-03'),
+                  ).then((value) {
+                    dateController.text = DateFormat.yMMMd().format(value!);
+                  });
+                  },
+                  // color: Colors.grey[400],
+                  width: 100,
+                ),
+                Container(
+                  width: 100,
+                  child: TextFormField(
+                    controller: dateController,
+                    decoration: const InputDecoration(
+      
+                        //label:Text("kkkk"),
+      
+                        hintText: ""),
+                  ),
+                ),
+              ],
             ),
-            Container(
-              width: 100,
-              child: TextFormField(
-                controller: dateController,
-                decoration: const InputDecoration(
+            const SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              children: [
+                CustomDefaultButton(
+                  height: 50,
+                  text: "Time",
+                  ontap: () {
+                    showTimePicker(
+                      context: context,
+                      initialTime: TimeOfDay.now(),
+                    ).then((value) {
+                      timeController.text =
+                          value!.format(context).split('/').first.toString();
 
-                    //label:Text("kkkk"),
-
-                    hintText: ""),
+                    });
+                  },
+                  // color: Colors.grey[400],
+                  width: 100,
+                ),
+                Container(
+                  width: 100,
+                  child: TextFormField(
+                    controller: timeController,
+                    onTap: () {},
+                    decoration: const InputDecoration(
+      
+                        //label:Text("kkkk"),
+      
+                        hintText: ""),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    CustomDrobDownMenuShortAction(),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    CustomText(
+                      text: "Short Action",
+                      color: Colors.black,
+                      fontSise: 15,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ],
+                )),
+            const SizedBox(
+              height: 18.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  CustomDrobDownMenuLongAction(),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  CustomText(
+                    text: "Long Action",
+                    color: Colors.black,
+                    fontSise: 15,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-        const SizedBox(
-          height: 10.0,
-        ),
-        Row(
-          children: [
-            CustomDefaultButton(
-              height: 50,
-              text: "Time",
-              ontap: () {
-                showTimePicker(
-                  context: context,
-                  initialTime: TimeOfDay.now(),
-                ).then((value) {
-                  timeController.text = value!.minute.toString();
-
-                  print(value.format(context));
-                });
-              },
-              // color: Colors.grey[400],
-              width: 100,
+            const SizedBox(
+              height: 18.0,
             ),
-            Container(
-              width: 100,
-              child: TextFormField(
-                onTap: () {},
-                decoration: const InputDecoration(
-
-                    //label:Text("kkkk"),
-
-                    hintText: ""),
-              ),
+            buildFormFeild(context, text: "Fasting", controller: fastingController),
+            const SizedBox(
+              height: 18.0,
             ),
-          ],
+           
+      
+            buildFormFeild(context, text: "Reminder", controller: fastingController),
+            const SizedBox(height: 18.0,),
+              Padding(
+               padding: const EdgeInsets.only(left: 10.0),
+               child: Row(
+                 mainAxisAlignment: MainAxisAlignment.start,
+                 children: [
+                   Container(
+                              width: MediaQuery.of(context).size.width*.6 ,
+                              padding: EdgeInsets.all(10.0),
+                              decoration: BoxDecoration(
+                                border: Border.all(color: ColorsApp.primaryColor),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: DropdownButton<String>(
+                                value: selectedItemPills,
+                                items: pillsList.map((e) {
+                                  return DropdownMenuItem(value: e, child: Text(e));
+                                }).toList(),
+                                onChanged: (String? value) {
+      
+                                  setState(() {
+                                    selectedItemPills = value!;
+                                    
+                                  });
+                                },
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 6.0,
+                            ),
+                            const CustomText(text: "Pills",color: Colors.black,fontSise: 17,fontWeight: FontWeight.bold,),
+                           
+                 ],
+               ),
+             ),
+             const SizedBox(height: 18.0,),
+              selectedItemPills=="other"?buildFormFeild(context,text: "Other",controller: otheController):Container()
+          ]),
         ),
-        const SizedBox(
-          height: 10,
-        ),
-        const Padding(
-            padding: EdgeInsets.only(left: 10),
-            child: CustomDrobDownMenuShortAction()),
-        const SizedBox(
-          height: 10.0,
-        ),
-        const Padding(
-          padding: EdgeInsets.only(left: 10),
-          child: CustomDrobDownMenuLongAction(),
-        )
-      ]),
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: ColorsApp.primaryColor,
         onPressed: () {},
@@ -123,4 +216,22 @@ class AddRecordScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget buildFormFeild(context,
+    {String? text, TextEditingController? controller}) {
+  return Padding(
+    padding: const EdgeInsets.only(left: 10),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        CustomTextField(
+          controller: controller,
+          width: MediaQuery.of(context).size.width * .5,
+          color: Colors.white12,
+          lableText: text,
+        ),
+      ],
+    ),
+  );
 }
