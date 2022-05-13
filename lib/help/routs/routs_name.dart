825f7f5c5b2863977e628ef0d399a54734223f1a
mohammedzidan <mohmmedzidane@gmail.com>
2022-05-13 12:17:36 +0300

    add report screen

--- lib/help/routs/routs_name.dart
+++ lib/help/routs/routs_name.dart
@@ -1,12 +1,14 @@
+import 'package:easysugar/view/screens/doctor_home_screen.dart';
+
 class RoutsNames {
   static const mainSplashScreen = '/';
   static const tapBarForRegestrationScreen = '/tap_bar_for_regestration_screen';
-  static const follwarScreen = '/follwar_screen';
   static const homeScreenForPatient = '/home_screen_for_patient';
-
   static const predictionScreen = '/prediction_screen';
   static const homeScreenForDoctor = '/dhome__screen_for_doctor';
   static const addRecordScreen = '/add_record_screen';
   static const settingsScreen = '/settinges_screen';
   static const emergencyScreen = '/emergency_screen';
+  static const reportScreen = '/report_Screen';
+  static const follwarScreen = '/follwar_screen';
 }
