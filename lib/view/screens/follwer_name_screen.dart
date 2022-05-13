b04118ffa1a1bd4bfc812f17658654a93912adeb
mohammedzidan <mohmmedzidane@gmail.com>
2022-05-13 15:59:46 +0300

    add follwer name screen

---
+++ lib/view/screens/follwer_name_screen.dart
@@ -0,0 +1,77 @@
+import 'package:easysugar/help/my_colors_app.dart';
+import 'package:easysugar/view/custom_widet/custom_curve.dart';
+import 'package:flutter/material.dart';
+
+import '../custom_widet/custom_background_curve.dart';
+import '../custom_widet/custom_text.dart';
+
+class FollwerNameScreen extends StatelessWidget {
+  const FollwerNameScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   elevation: 0.0,
+      //     backgroundColor:ColorsApp.primaryColor,
+      //     title: ),
+      body: Stack(
+        children: [
+          CustomBackground(),
+          SafeArea(
+            child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.arrow_back_ios)),
+          ),
+          Container(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const CustomText(
+                  text: "FOLLWERS",
+                  color: Colors.black,
+                  fontSise: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                buildFollwerNameItem("Mohmmed Zidane"),
+                buildFollwerNameItem("Menaa"),
+                buildFollwerNameItem("Basmala")
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget buildFollwerNameItem(text) {
+  return Column(
+    children: [
+      Container(
+        height: 40,
+        width: 200,
+        decoration: BoxDecoration(
+            color: ColorsApp.primaryColor,
+            borderRadius: BorderRadius.circular(20)),
+        child: Center(
+          child: CustomText(
+            text: text,
+            color: Colors.white,
+            fontSise: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      const SizedBox(
+        height: 25,
+      ),
+    ],
+  );
+}
